4857192c08bb6fac66ee2f8b90f81dc1c698134b
Florian Uekermann <florian@uekermann.me>
2024-02-23 22:58:53 +0100

    fix doc strings

--- src/node.rs
+++ src/node.rs
@@ -1,21 +1,21 @@
 pub trait NodeExt: AsRef<web_sys::Node> {
-    // shorthand for `self.append_child(&child).unwrap()`
+    /// shorthand for `self.append_child(&child).unwrap()`
     fn child(&self, child: impl AsRef<web_sys::Node>) -> &Self {
         self.as_ref().append_child(child.as_ref()).unwrap();
         self
     }
-    // removes all child nodes
+    /// removes all child nodes
     fn clear(&self) {
         let node = self.as_ref();
         while let Some(child) = node.last_child() {
             node.remove_child(&child).unwrap();
         }
     }
-    // shorthand for `self.parent_node().unwrap().replace_child(&new, self).unwrap()`
+    /// shorthand for `self.parent_node().unwrap().replace_child(&new, self).unwrap()`
     fn replace(&self, new: impl AsRef<web_sys::Node>) {
         self.as_ref().parent_node().unwrap().replace_child(new.as_ref(), self.as_ref()).unwrap();
     }
-    // like [Self::replace], but also overwrites `self` with the new element and returns the original self.
+    /// like [Self::replace], but also overwrites `self` with the new element and returns the original `self`.
     fn overwrite(&mut self, new: Self) -> Self
     where
         Self: Sized,
