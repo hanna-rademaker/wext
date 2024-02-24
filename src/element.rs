5b9f5ef2f24554ee6318abb7d4639f38b1b867d4
Florian Uekermann <florian@uekermann.me>
2024-02-24 20:03:43 +0100

    add class and attr

--- src/element.rs
+++ src/element.rs
@@ -1,3 +1,14 @@
-pub trait ElementExt {}
+pub trait ElementExt: AsRef<web_sys::Element> {
+    /// Add `c` to the class list
+    fn class(&self, c: impl AsRef<str>) -> &Self {
+        self.as_ref().class_list().add_1(c.as_ref()).unwrap();
+        self
+    }
+    /// Set the attribute `name` to the specified value
+    fn attr(&self, name: impl AsRef<str>, value: impl AsRef<str>) -> &Self {
+        self.as_ref().set_attribute(name.as_ref(), value.as_ref()).unwrap();
+        self
+    }
+}
 
 impl<T: AsRef<web_sys::Element>> ElementExt for T {}
