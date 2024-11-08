9764b6b976ff32d85a0b55250db6d6a7f8cb2764
Hanna Rademaker <hannarademaker@gmail.com>
2024-11-08 18:21:16 +0100

    work in progress

--- examples/todo/src/main.rs
+++ examples/todo/src/main.rs
@@ -1,27 +1,78 @@
 use std::{cell::RefCell, rc::Rc};
 
 use gloo::{events::EventListener, utils::body};
-use log::info;
+use web_sys::HtmlDivElement;
 use wext::{
-    html::short::{form, h1, input},
+    html::short::{div, form, h1, input, p},
     prelude::*,
 };
 
 #[derive(Debug)]
 struct Item {
+    root: HtmlDivElement,
     todo: String,
     done: bool,
+    _els: Vec<EventListener>,
 }
 
 impl Item {
     fn new(todo: String) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Self { todo, done: false }))
+        Rc::<RefCell<Item>>::new_cyclic(|weak| {
+            let root = div();
+            let checkbox = input().attr("type", "checkbox");
+            root.child(&div().child(&checkbox).child(&p().txt(todo.as_str())));
+            let mut els = Vec::new();
+            els.push(weak.event_listener(&checkbox, "change", |this, _| {
+                this.update();
+            }));
+            RefCell::new(Self {
+                root,
+                todo,
+                done: false,
+                _els: els,
+            })
+        })
     }
+    fn update(&mut self) {}
 }
 
 struct State {
     todo_list: Vec<Rc<RefCell<Item>>>,
+    container: HtmlDivElement,
     _els: Vec<EventListener>,
+}
+
+impl State {
+    fn new() -> Rc<RefCell<Self>> {
+        let state = Rc::<RefCell<State>>::new_cyclic(|weak| {
+            let form = form();
+            let input_field = input();
+            form.child(&input_field);
+            let container = div();
+            body().child(&form).child(&container);
+            let mut els = Vec::new();
+            els.push(weak.event_listener(&form, "submit", move |state, e| {
+                e.prevent_default();
+                let item = Item::new(input_field.value());
+                state.todo_list.push(item);
+                input_field.set_value("");
+                state.update();
+            }));
+            RefCell::new(State {
+                todo_list: Vec::new(),
+                container,
+                _els: els,
+            })
+        });
+        state
+    }
+    fn update(&self) {
+        self.container.clear();
+        for item in &self.todo_list {
+            let item = item.borrow();
+            self.container.child(&item.root);
+        }
+    }
 }
 
 fn main() {
@@ -30,25 +81,7 @@ fn main() {
     log::set_max_level(log::LevelFilter::Info);
 
     body().child(&h1().txt("todos").css("color", "red"));
-    let form = form();
-    let input_field = input();
-    form.child(&input_field);
-    body().child(&form);
-
-    let state = Rc::<RefCell<State>>::new_cyclic(|weak| {
-        let mut els = Vec::new();
-        els.push(weak.event_listener(&form, "submit", move |state, e| {
-            e.prevent_default();
-            let item = Item::new(input_field.value());
-            state.todo_list.push(item);
-            input_field.set_value("");
-            info!("{:?}", state.todo_list);
-        }));
-        RefCell::new(State {
-            todo_list: Vec::new(),
-            _els: els,
-        })
-    });
+    let state = State::new();
 
     std::mem::forget(state)
 }
